8452c859142a139483decbc7e3b956f13713360a
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-20 22:19:57 +0100

    feat: update Nextjs _app and _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import TopBar from "@/components/TopBar/TopBar";
+import { AuthenticationProvider } from "@/context/AuthenticationContext";
+import { CartProvider } from "@/context/CartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // This is a workaround for the self-signed certificate issue in development
+  if (process.env.NODE_ENV === "development") {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  } else {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <AuthenticationProvider>
+        <CartProvider>
+          <TopBar />
+          <Component {...pageProps} />
+        </CartProvider>
+      </AuthenticationProvider>
+    </>
+  );
 }
