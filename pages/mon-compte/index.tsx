66d69a1452dd3ea741ec81f72ae88c87a1c673b4
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-20 23:27:50 +0100

    feat: add user account page

---
+++ pages/mon-compte/index.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import ClientInformations from "@/components/ClientInformations/ClientInformations";
+import { useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { AuthenticationContext } from "@/context/AuthenticationContext";
+
+export default function AccountPage() {
+  const loggedInClient = useContext(AuthenticationContext);
+  const router = useRouter();
+
+  // If the user is not logged in, do not display the page and redirect to the home page.
+  useEffect(() => {
+    if (!loggedInClient) {
+      router.push("/");
+    }
+  }, [loggedInClient, router]);
+
+  return (
+    <>
+      <Head>
+        <title>Foodles | Mon compte</title>
+      </Head>
+      <main>
+        <h1 className="text-center text-orange">Mon compte</h1>
+        <section>{loggedInClient && <ClientInformations client={loggedInClient} />}</section>
+      </main>
+    </>
+  );
+}
