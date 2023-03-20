2f2c65427ec696ebab54ea1e00ac379a61be2043
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-20 23:28:05 +0100

    feat: add foodles logo on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import OpenShopButton from "@/components/OpenShopButton/OpenShopButton";
+import logoImage from "@/public/images/logo.png";
 
 export default function HomePage() {
   return (
@@ -10,7 +11,12 @@ export default function HomePage() {
         <meta name="description" content="Bienvenue sur le site de Foodles où vous pourrez commander en toute simplicité des plats en tout genre pour vous régaler rapidement ! Nos produits sont de qualité, frais et préparés avec le plus grand soin. Nos circuits courts vous garantissent des produits de saison biologiques et locaux et le respect de l'environnement est au cœur de notre démarche." />
       </Head>
       <main>
-        <OpenShopButton />
+        <div className="w-full flex justify-center align-center">
+          <Image src={logoImage} alt="Logo de Foodles" width={200} height={200} />
+        </div>
+        <section>
+          <OpenShopButton />
+        </section>
       </main>
     </>
   );
