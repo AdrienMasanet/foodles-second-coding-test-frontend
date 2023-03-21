328cdaeee52028c398f00c5353c522d41d5f07b3
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-21 23:32:19 +0100

    refactor: wrong name function useCart is now useClientList

--- hooks/useClientList.tsx
+++ hooks/useClientList.tsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { ClientListContext, ClientListUpdateContext } from "@/context/ClientListContext";
 
-const useCart = () => {
+const useClientList = () => {
   const clientList = useContext(ClientListContext);
   const { refreshClientList } = useContext(ClientListUpdateContext);
 
   return { clientList, refreshClientList };
 };
 
-export default useCart;
+export default useClientList;
