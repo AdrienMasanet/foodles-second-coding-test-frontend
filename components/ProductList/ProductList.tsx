6151db63f585f41ea3a06bd9e7e32d8ba3191277
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-21 15:17:11 +0100

    feat: center no product found text

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -12,10 +12,10 @@ type ProductListProps = {
  */
 const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className={styles.container}>
-      {products.length > 0 && products.map((product) => <ProductListItem key={product.id} product={product} />)}
+    <>
       {products.length === 0 && <p className="text-center text-orange">Aucun produit en stock actuellement...</p>}
-    </div>
+      <div className={styles.container}>{products.length > 0 && products.map((product) => <ProductListItem key={product.id} product={product} />)}</div>
+    </>
   );
 };
 
