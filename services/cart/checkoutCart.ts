5ea968b69258ce5c04ebc7a0bf0ec6282801c49e
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-22 23:35:28 +0100

    docs: add jsdoc

--- services/cart/checkoutCart.ts
+++ services/cart/checkoutCart.ts
@@ -1,4 +1,12 @@
-export async function checkoutCart(cart: { [key: string]: number }) {
+/**
+ * Sends the cart dictionary as a JSON object to the API and returns the client's new credit amount after checkout.
+ * @async
+ * @function
+ * @param {Object.<string, number>} cart - A dictionary representing the cart with items as keys and their quantities as values
+ * @returns {Promise<number>} - A promise that resolves to the client's new credit amount after the order has been placed
+ * @throws {Error} - If an error occurs during the request or if the response status is not 200
+ */
+async function checkoutCart(cart: { [key: string]: number }) {
   // Send the cart dictionnary as JSON object to the API.
   return fetch(process.env.NEXT_PUBLIC_API_URL + "orders/new", {
     method: "POST",
